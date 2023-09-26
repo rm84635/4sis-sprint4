d71f1ec9058ae0766be56eacb69836ab166de9f8
RM84635 <rm84635@fiap.com.br>
2023-09-26 15:52:15 -0300

    Adiciona página de oportunidades

--- src/app.js
+++ src/app.js
@@ -54,6 +54,49 @@ let fakeUser = {
   ]
 };
 
+const oportunidades = [
+  {
+    company: 'Google',
+    job: 'UX/UI designer',
+    description: 'Estamos procurando profissionais com experiência em UX/UI design.'
+  },
+  {
+    company: 'FIAP',
+    job: 'DevOps e Infra',
+    description: 'Precisamos de profissionais especializados em DevOps e infraestrutura de sistemas.'
+  },
+  {
+    company: 'Amazon',
+    job: 'QA Tester',
+    description: 'À procura de QA Testers para garantir a qualidade e segurança dos nossos sistemas.'
+  },
+  {
+    company: 'InfoSafe',
+    job: 'Especialista em segurança',
+    description: 'Somos uma nova startup e estamos buscando especialistas em segurança digital.'
+  },
+  {
+    company: 'Twitter',
+    job: 'API designer',
+    description: 'Procura-se profissionais com experiência em desenvolver APIs REST.'
+  },
+  {
+    company: 'Hirota Mercados',
+    job: 'Técnico de T.I.',
+    description: 'Técnico de T.I. para manter os sistemas da rede de mercados.'
+  },
+  {
+    company: 'Apple',
+    job: 'Desenvolvedor iOS mobile',
+    description: 'Em busca de desenvolvedores iOS mobile com experiência em Swift e Objective-C.'
+  },
+  {
+    company: 'FIAP',
+    job: 'Desenvolvedor mobile',
+    description: 'Procuramos desenvolvedores talentosos focados em Kotlin e Flutter.'
+  },
+];
+
 const renderPage = (pageName, pageTitle, res, obj) => {
   if (!obj.hasOwnProperty('renderHeader')) {
     obj.renderHeader = true;
@@ -63,8 +106,11 @@ const renderPage = (pageName, pageTitle, res, obj) => {
     obj.renderFooter = true;
   }
 
+  obj.userLogged = userLogged;
+
   if (userLogged) {
     obj.userName = 'Pedro da Silva';
+    obj.user = fakeUser;
   }
 
   obj.pageCSS = `/static/css/pages/${pageName}.css`;
@@ -82,7 +128,7 @@ app.use (bodyParser.urlencoded({extended : true}));
 app.use('/static', express.static(path.join(__dirname, 'static')))
 
 app.get('/', (_, res) => {
-  renderPage('index', 'Início', res, {userLogged: userLogged});
+  renderPage('index', 'Início', res, {});
 });
 
 app.get('/login', (_, res) => {
@@ -94,17 +140,36 @@ app.get('/login', (_, res) => {
   });
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', (_, res) => {
   userLogged = true;
   res.redirect('/perfil');
 });
 
+app.get('/logout', (_, res) => {
+  userLogged = false;
+  res.redirect('/');
+});
+
 app.get('/perfil', (_, res) => {
   if (userLogged) {
-    renderPage('perfil', 'Meu Perfil', res, {user: fakeUser, userLogged: userLogged});
+    renderPage('perfil', 'Meu Perfil', res, {});
   } else {
     res.redirect('/');
   }
+});
+
+app.get('/perfil', (_, res) => {
+  if (userLogged) {
+    renderPage('perfil', 'Meu Perfil', res, {});
+  } else {
+    res.redirect('/');
+  }
+});
+
+app.get('/oportunidades', (_, res) => {
+  renderPage('oportunidades', 'Oportunidades', res, {
+    oportunidades: oportunidades
+  });
 });
 
 app.listen(PORT, _ => {
